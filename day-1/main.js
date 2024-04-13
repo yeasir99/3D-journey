c14625e21d59bb867291e690d298ae702d9ea02c
Yeasir Ahamed <yahamed007@gmail.com>
2024-04-14 02:12:36 +0600

    test sin con tan math function for animation

--- day-1/main.js
+++ day-1/main.js
@@ -8,13 +8,13 @@ const scene = new THREE.Scene();
 
 // setup Mesh
 
-const geomatry = new THREE.BoxGeometry(1, 1, 1);
+const geomatry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
 const meterials = new THREE.MeshBasicMaterial({ color: 'red' });
 
-const mesh = new THREE.Mesh(geomatry, meterials);
+const redMash = new THREE.Mesh(geomatry, meterials);
 
-scene.add(mesh);
+scene.add(redMash);
 
 // setup camera
 
@@ -25,7 +25,7 @@ const size = {
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 
-camera.position.z = 2;
+camera.position.z = 3;
 
 scene.add(camera);
 
@@ -44,7 +44,17 @@ const clock = new THREE.Clock();
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  mesh.rotation.y = elapsedTime * Math.PI;
+  // using sin fn
+
+  // mesh.position.x = Math.sin(elapsedTime) * 2;
+  // using cos fn
+  // mesh.position.y = Math.cos(elapsedTime) * 2;
+
+  // using tan fn
+  // mesh.position.x = Math.tan(elapsedTime) * 2;
+  // mesh.position.y = Math.tan(elapsedTime) * 2;
+
+  redMash.rotation.y = elapsedTime * Math.PI;
   renderer.render(scene, camera);
   window.requestAnimationFrame(animate);
 };
