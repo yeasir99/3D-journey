cc37a112fd2c47cd1bb465a9c7f00b6c022e710e
Yeasir Ahamed <yahamed007@gmail.com>
2024-04-15 19:33:29 +0600

    add lookAt method animation with cursor move

--- day-2/main.js
+++ day-2/main.js
@@ -3,5 +3,6 @@ import initial from './utils/initial';
 import animation from './utils/animation';
 import responsive from './utils/responsive';
 import lookAt from './utils/lookAt';
+import lookAtCursorAnimation from './utils/lookAtWithCursor';
 
-lookAt();
+lookAtCursorAnimation();
