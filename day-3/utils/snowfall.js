303940f29ad4f3072521b048a9b7612499f955cc
Yeasir Ahamed <yahamed007@gmail.com>
2024-04-17 00:10:04 +0600

    add animation

--- day-3/utils/snowfall.js
+++ day-3/utils/snowfall.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const geomatry = new THREE.PlaneGeometry(1, 1);
+const geomatry = new THREE.PlaneGeometry(0.5, 0.5);
 
 const materials = new THREE.MeshBasicMaterial({ color: 'red' });
 
@@ -16,16 +17,31 @@ const aspect = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, aspect.x / aspect.y);
-camera.position.z = 4;
+camera.position.z = 2;
 
 scene.add(camera);
-
 const canvas = document.querySelector('.webgl');
+
+const controls = new OrbitControls(camera, canvas);
+
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(aspect.x, aspect.y);
 
+window.addEventListener('resize', () => {
+  aspect.x = window.innerWidth;
+  aspect.y = window.innerHeight;
+
+  camera.aspect = aspect.x / aspect.y;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(aspect.x, aspect.y);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
 const snowfall = () => {
+  controls.update();
   renderer.render(scene, camera);
+  window.requestAnimationFrame(snowfall);
 };
 
 export default snowfall;
