5e43381611c9eab6f392042aa552ea5be8a5f4aa
Yeasir Ahamed <yahamed007@gmail.com>
2024-04-17 01:28:05 +0600

    add show animation

--- day-3/utils/snowfall.js
+++ day-3/utils/snowfall.js
@@ -3,20 +3,40 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const geomatry = new THREE.PlaneGeometry(0.5, 0.5);
+const geomatry = new THREE.BufferGeometry();
 
-const materials = new THREE.MeshBasicMaterial({ color: 'red' });
+const verticesAmound = 20000;
 
-const mesh = new THREE.Mesh(geomatry, materials);
+const positionArray = new Float32Array(verticesAmound * 3);
 
-scene.add(mesh);
+for (let i = 0; i < verticesAmound * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 4;
+}
+
+const textureLoader = new THREE.TextureLoader();
+
+const textureImage = textureLoader.load('/alphaSnow.jpg');
+
+console.log(textureImage);
+
+geomatry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3));
+
+const materials = new THREE.PointsMaterial();
+materials.size = 0.02;
+materials.transparent = true;
+materials.alphaMap = textureImage;
+materials.depthTest = false;
+
+const points = new THREE.Points(geomatry, materials);
+
+scene.add(points);
 
 const aspect = {
   x: window.innerWidth,
   y: window.innerHeight,
 };
 
-const camera = new THREE.PerspectiveCamera(75, aspect.x / aspect.y);
+const camera = new THREE.PerspectiveCamera(75, aspect.x / aspect.y, 0.01, 100);
 camera.position.z = 2;
 
 scene.add(camera);
@@ -24,7 +44,7 @@ const canvas = document.querySelector('.webgl');
 
 const controls = new OrbitControls(camera, canvas);
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(aspect.x, aspect.y);
 
 window.addEventListener('resize', () => {
@@ -38,7 +58,12 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+const clock = new THREE.Clock();
+
 const snowfall = () => {
+  const eleapsedTime = clock.getElapsedTime();
+  points.rotation.y = -eleapsedTime * 0.06;
+  points.rotation.x = eleapsedTime * 0.06;
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(snowfall);
